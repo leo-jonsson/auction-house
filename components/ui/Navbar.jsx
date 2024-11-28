6f38dbf003e9c46f3c937eb89464237660ccd21a
Leo Jonsson <146437864+leomrgreen@users.noreply.github.com>
2024-11-28 15:26:56 +0100

    navbar change and videoplayer back-blur

--- components/ui/Navbar.jsx
+++ components/ui/Navbar.jsx
@@ -33,6 +33,7 @@ const Navbar = () => {
   const api = new ProfileAPI();
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const pathname = usePathname();
 
   useEffect(() => {
     const fetchUserProfile = async () => {
@@ -51,158 +52,187 @@ const Navbar = () => {
     fetchUserProfile();
   }, []);
 
+  const links = user
+    ? [
+        { name: "Home", href: "/" },
+        { name: "Listings", href: "/listings" },
+        { name: "Create listing", href: "/listings/post" },
+        { name: "Profile", href: `/user/${user?.name}` },
+      ]
+    : [
+        { name: "Home", href: "/" },
+        { name: "Listings", href: "/listings" },
+      ];
+
   return (
     <nav className="w-full top-0 border-b shadow-sm sm:shadow-none bg-background/70 backdrop-blur-md sticky flex items-center justify-between p-2 z-[50]">
       <div className="flex justify-between max-w-[78rem] w-full mx-auto">
         <span className="flex items-center gap-1">
           <Logo />
           <span className="font-bold">AUCSOME</span>
+          <div className="flex items-center gap-5">
+            <ul className="flex gap-5 ml-5 text-muted-foreground">
+              {links.map((link) => {
+                const isActive = pathname === link.href; // Använd pathname som redan hämtats
+                return (
+                  <li key={link.name} className="sm:flex hidden">
+                    <Link
+                      href={link.href}
+                      className={`transition-colors ${
+                        isActive ? "text-foreground" : "hover:text-foreground"
+                      }`}
+                    >
+                      {link.name}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </span>
 
         <div className="flex items-center gap-5">
-          <ul className="flex gap-5 ml-5 text-muted-foreground">
-            {[
-              { name: "Home", href: "/", ref: "/" },
-              { name: "Listings", href: "/listings", ref: "/listings" },
-            ].map((link) => {
-              const isActive = usePathname() === link.ref;
-              return (
-                <li key={link.name} className="sm:flex hidden">
-                  <Link
-                    href={link.href}
-                    className={`transition-colors ${
-                      isActive
-                        ? "text-foreground font-bold"
-                        : "hover:text-foreground"
-                    }`}
-                  >
-                    {link.name}
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
           {isLoading ? (
             <Skeleton className="w-9 aspect-square rounded-full" />
           ) : user ? (
-            <Sheet>
-              <SheetTrigger asChild>
-                <Avatar className="cursor-pointer">
+            <div>
+              <div className="sm:flex gap-2 items-center hidden">
+                <Avatar className="size-8">
                   <AvatarImage
                     src={user.avatar?.url}
                     alt={user.avatar?.alt || "User avatar"}
                   />
                   <AvatarFallback>{user.name}</AvatarFallback>
                 </Avatar>
-              </SheetTrigger>
-              <SheetContent className="flex flex-col justify-between">
-                <div className="grid gap-5">
-                  <SheetHeader>
-                    <SheetTitle>
-                      <span className="flex items-center gap-2">
-                        <Avatar className="cursor-pointer">
-                          <AvatarImage
-                            src={user.avatar?.url}
-                            alt={user.avatar?.alt || "User avatar"}
-                          />
-                          <AvatarFallback>{user.name}</AvatarFallback>
-                        </Avatar>
-                        {user.name}
-                      </span>
-                    </SheetTitle>
-                  </SheetHeader>
-                  <ul className="text-muted-foreground">
-                    <li className="hover:text-foreground transition-colors w-full  p-2 rounded-lg flex items-center gap-2">
-                      <FaCoins /> {user.credits} Credits
-                    </li>
-                    <li>
-                      <SheetClose asChild>
-                        <Link
-                          href={`/user/${user.name}`}
-                          className="hover:bg-muted hover:text-foreground transition-colors w-full  p-2 rounded-lg flex items-center gap-2"
-                        >
-                          <FaRegUser /> Profile
-                        </Link>
-                      </SheetClose>
-                    </li>
-                    <li>
-                      <SheetClose asChild>
-                        <Link
-                          href="/"
-                          className="hover:bg-muted hover:text-foreground transition-colors w-full  p-2 rounded-lg flex items-center gap-2"
-                        >
-                          <FaRegChartBar /> My Biddings
-                        </Link>
-                      </SheetClose>
-                    </li>
+                <ModeToggle />
+              </div>
+              <div className="sm:hidden block">
+                <Sheet>
+                  <SheetTrigger asChild>
+                    <Avatar className="cursor-pointer">
+                      <AvatarImage
+                        src={user.avatar?.url}
+                        alt={user.avatar?.alt || "User avatar"}
+                      />
+                      <AvatarFallback>{user.name}</AvatarFallback>
+                    </Avatar>
+                  </SheetTrigger>
+                  <SheetContent className="flex flex-col justify-between">
+                    <div className="grid gap-5">
+                      <SheetHeader>
+                        <SheetTitle>
+                          <span className="flex items-center gap-2">
+                            <Avatar className="cursor-pointer">
+                              <AvatarImage
+                                src={user.avatar?.url}
+                                alt={user.avatar?.alt || "User avatar"}
+                              />
+                              <AvatarFallback>{user.name}</AvatarFallback>
+                            </Avatar>
+                            {user.name}
+                          </span>
+                        </SheetTitle>
+                      </SheetHeader>
+                      <ul className="text-muted-foreground">
+                        <li className="hover:text-foreground transition-colors w-full  p-2 rounded-lg flex items-center gap-2">
+                          <FaCoins /> {user.credits} Credits
+                        </li>
+                        <li>
+                          <SheetClose asChild>
+                            <Link
+                              href={`/user/${user.name}`}
+                              className="hover:bg-muted hover:text-foreground transition-colors w-full  p-2 rounded-lg flex items-center gap-2"
+                            >
+                              <FaRegUser /> Profile
+                            </Link>
+                          </SheetClose>
+                        </li>
+                        <li>
+                          <SheetClose asChild>
+                            <Link
+                              href="/"
+                              className="hover:bg-muted hover:text-foreground transition-colors w-full  p-2 rounded-lg flex items-center gap-2"
+                            >
+                              <FaRegChartBar /> My Biddings
+                            </Link>
+                          </SheetClose>
+                        </li>
 
-                    <li>
-                      <SheetClose asChild>
-                        <Link
-                          href="/"
-                          className="hover:bg-muted hover:text-foreground transition-colors w-full  p-2 rounded-lg flex items-center gap-2"
-                        >
-                          <FaHome />
-                          Home
-                        </Link>
-                      </SheetClose>
-                    </li>
-                    <li>
-                      <SheetClose asChild>
-                        <Link
-                          href="/about"
-                          className="hover:bg-muted hover:text-foreground transition-colors w-full  p-2 rounded-lg flex items-center gap-2"
-                        >
-                          <FaInfoCircle />
-                          About
-                        </Link>
-                      </SheetClose>
-                    </li>
-                    <li>
-                      <SheetClose asChild>
-                        <Link
-                          href="/contact"
-                          className="hover:bg-muted hover:text-foreground transition-colors w-full  p-2 rounded-lg flex items-center gap-2"
-                        >
-                          <FaPhone />
-                          Contact
-                        </Link>
-                      </SheetClose>
-                    </li>
-                    <li>
-                      <SheetClose asChild>
-                        <Link
-                          href="/listings/post"
-                          className="hover:bg-muted hover:text-foreground transition-colors w-full  p-2 rounded-lg flex items-center gap-2"
-                        >
-                          <FaPlusCircle />
-                          Create listing
-                        </Link>
-                      </SheetClose>
-                    </li>
-                  </ul>
-                </div>
-                <div className="flex">
-                  <span className="mr-auto">
-                    <SignOutBtn />
-                  </span>
-                  <span className="ml-auto">
-                    <ModeToggle />
-                  </span>
-                </div>
-              </SheetContent>
-            </Sheet>
+                        <li>
+                          <SheetClose asChild>
+                            <Link
+                              href="/"
+                              className="hover:bg-muted hover:text-foreground transition-colors w-full  p-2 rounded-lg flex items-center gap-2"
+                            >
+                              <FaHome />
+                              Home
+                            </Link>
+                          </SheetClose>
+                        </li>
+                        <li>
+                          <SheetClose asChild>
+                            <Link
+                              href="/about"
+                              className="hover:bg-muted hover:text-foreground transition-colors w-full  p-2 rounded-lg flex items-center gap-2"
+                            >
+                              <FaInfoCircle />
+                              About
+                            </Link>
+                          </SheetClose>
+                        </li>
+                        <li>
+                          <SheetClose asChild>
+                            <Link
+                              href="/contact"
+                              className="hover:bg-muted hover:text-foreground transition-colors w-full  p-2 rounded-lg flex items-center gap-2"
+                            >
+                              <FaPhone />
+                              Contact
+                            </Link>
+                          </SheetClose>
+                        </li>
+                        <li>
+                          <SheetClose asChild>
+                            <Link
+                              href="/listings/post"
+                              className="hover:bg-muted hover:text-foreground transition-colors w-full  p-2 rounded-lg flex items-center gap-2"
+                            >
+                              <FaPlusCircle />
+                              Create listing
+                            </Link>
+                          </SheetClose>
+                        </li>
+                      </ul>
+                    </div>
+                    <div className="flex">
+                      <span className="mr-auto">
+                        <SignOutBtn />
+                      </span>
+                      <span className="ml-auto">
+                        <ModeToggle />
+                      </span>
+                    </div>
+                  </SheetContent>
+                </Sheet>
+              </div>
+            </div>
           ) : (
             <div className="flex items-center gap-4">
-              <span className="sm:flex hidden">
-                <ModeToggle />
-              </span>
               <Link
                 href="/auth/login"
-                className="px-4 py-1 bg-primary text-white rounded-full sm:flex hidden"
+                className="px-4 py-1 bg-background text-foreground border hover:bg-muted transition-colors rounded-lg sm:flex hidden"
               >
                 Login
               </Link>
+              <Link
+                href="/auth/register"
+                className="px-4 py-1 bg-primary text-white rounded-lg sm:flex hidden"
+              >
+                Create an account
+              </Link>
+              <span className="sm:flex hidden">
+                <ModeToggle />
+              </span>
               <Sheet>
                 <SheetTrigger asChild>
                   <Button variant="icon" className="sm:hidden flex">
--- components/ui/VideoPlayer.jsx
+++ components/ui/VideoPlayer.jsx
@@ -2,30 +2,38 @@ import React from "react";
 
 const VideoPlayer = () => {
   return (
-    <div className="w-full -translate-y-14 flex-flex-col border rounded-lg overflow-hidden">
-      <div className="flex items-center gap-2 p-2 border-b">
-        <span className="size-3 rounded-full bg-muted-foreground" />
-        <span className="size-3 rounded-full bg-muted-foreground" />
-        <span className="size-3 rounded-full bg-muted-foreground" />
+    <div className="relative w-full h-full flex items-center justify-center">
+      {/* Background Blur/Gradient Effect */}
+      <div className="absolute inset-0 w-full h-full bg-primary/80 dark:bg-primary/50 blur-3xl -translate-y-20 rounded-3xl aspect-square m-auto"></div>
+
+      {/* Video Player Container */}
+      <div className="relative z-10 w-full -translate-y-14 flex flex-col rounded-lg overflow-hidden bg-card p-0 border">
+        {/* Mock Controls */}
+        <div className="flex items-center gap-2 p-2">
+          <span className="size-3 rounded-full bg-muted-foreground dark:bg-muted" />
+          <span className="size-3 rounded-full bg-muted-foreground dark:bg-muted" />
+          <span className="size-3 rounded-full bg-muted-foreground dark:bg-muted" />
+        </div>
+        {/* Video Player */}
+        <video
+          width="1200"
+          height="700"
+          preload="none"
+          autoPlay
+          loop
+          muted
+          className="mx-auto w-full"
+        >
+          <source src="/test.mp4" type="video/mp4" />
+          <track
+            src="/path/to/captions.vtt"
+            kind="subtitles"
+            srcLang="en"
+            label="English"
+          />
+          Your browser does not support the video tag.
+        </video>
       </div>
-      <video
-        width="1200"
-        height="700"
-        preload="none"
-        autoPlay
-        loop
-        muted
-        className="mx-auto w-full"
-      >
-        <source src="/test.mp4" type="video/mp4" />
-        <track
-          src="/path/to/captions.vtt"
-          kind="subtitles"
-          srcLang="en"
-          label="English"
-        />
-        Your browser does not support the video tag.
-      </video>
     </div>
   );
 };
